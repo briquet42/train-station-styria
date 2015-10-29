d1d69992c6b622fc2274073980a6e706e8775a7c
vickytonal <vickylind@live.at>
2015-10-29 21:17:04 +0100

    Aussehen verbessert, neue Seiten hinzugefügt

---
+++ Train_Stations/app/src/main/java/mobappdev/itm/fhj/at/train_stations/EnterNameActivity.java
@@ -0,0 +1,15 @@
+package mobappdev.itm.fhj.at.train_stations;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Viktoria on 29.10.2015.
+ */
+public class EnterNameActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enter_name);
+    }
+}
--- Train_Stations/app/src/main/java/mobappdev/itm/fhj/at/train_stations/StartActivity.java
+++ Train_Stations/app/src/main/java/mobappdev/itm/fhj/at/train_stations/StartActivity.java
@@ -1,7 +1,9 @@
 package mobappdev.itm.fhj.at.train_stations;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * Created by Viktoria on 29.10.2015.
@@ -12,5 +14,15 @@ public class StartActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+    }
+
+    public void openCalcMain(View v){
+        Intent i=new Intent(this,MainActivity.class);
+        startActivity(i);
+    }
+
+    public void openEnterName(View v){
+        Intent i=new Intent(this,EnterNameActivity.class);
+        startActivity(i);
     }
 }
