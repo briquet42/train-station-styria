a37e1817b9fabced277facbcee12329a477f6618
vickytonal <vickylind@live.at>
2015-10-29 20:33:32 +0100

    Neuer Startbildschirm

---
+++ Train_Stations/app/src/main/java/mobappdev/itm/fhj/at/train_stations/StartActivity.java
@@ -0,0 +1,16 @@
+package mobappdev.itm.fhj.at.train_stations;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Viktoria on 29.10.2015.
+ */
+public class StartActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+}
