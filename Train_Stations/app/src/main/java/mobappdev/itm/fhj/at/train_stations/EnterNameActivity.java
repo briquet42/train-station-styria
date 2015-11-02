3018708863feef994206ebbed4c18b9a01e7f0a8
vickytonal <vickylind@live.at>
2015-11-02 22:10:16 +0100

    anzeigen der Züge einer fixen Zeit in Kapfenberg

--- Train_Stations/app/src/main/java/mobappdev/itm/fhj/at/train_stations/EnterNameActivity.java
+++ Train_Stations/app/src/main/java/mobappdev/itm/fhj/at/train_stations/EnterNameActivity.java
@@ -1,23 +1,36 @@
 package mobappdev.itm.fhj.at.train_stations;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebView;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 /**
  * Created by Viktoria on 29.10.2015.
  */
-public class EnterNameActivity extends Activity {
+public class EnterNameActivity extends Activity implements ICallBack{
     private String STATION="next railway station";
     private EditText inputStation;
+    private TextView outputDisplay;
     private SharedPreferences prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter_name);
+
+        outputDisplay=(TextView)findViewById(R.id.txtOutput);
 
         inputStation=(EditText)findViewById(R.id.txtStation);
         prefs = getSharedPreferences("Station", 0);
@@ -26,9 +39,43 @@ public class EnterNameActivity extends Activity {
     }
 
     public void getDisplay(View v){
+        String sUrl = "http://fahrplan.oebb.at/bin/stboard.exe/dn?L=vs_scotty.vs_liveticker&evaId=8100031&boardType=dep&time=11:31&productsFilter=1111111111111111&additionalTime=0&disableEquivs=yes&maxJourneys=10&outputMode=tickerDataOnly&start=yes&selectDate=today\n";
+        HttpHelper helper = new HttpHelper();
+        helper.setCallback(this);
+        helper.execute(sUrl);
 
+        //save the input as standard
         SharedPreferences.Editor edit = prefs.edit();
         edit.putString(STATION, inputStation.getText().toString());
         edit.commit();
+    }
+
+    @Override
+    public void handleJSonString(String jsonString) {
+        try {
+            JSONObject jsonObject = new JSONObject(jsonString.substring(14));
+            JSONArray ja = jsonObject.getJSONArray("journey");
+           // String weatherStr = ja.getJSONObject(0).getString("st") + ", " + ja.getJSONObject(0).getString("st");
+          //  this.outputDisplay.setText(weatherStr);
+            //this.outputDisplay.setText((CharSequence) ja);
+
+            final int n = ja.length();
+            for (int i = 0; i < n; ++i) {
+                final JSONObject person = ja.getJSONObject(i);
+                String train=person.getString("ti")+"; "+person.getString("pr")+"; "+person.getString("lastStop");
+                this.outputDisplay.setText(outputDisplay.getText()+train+"\n");
+                System.out.println(person.getString("ti"));
+                System.out.println(person.getString("pr"));
+                System.out.println(person.getString("lastStop"));
+            }
+
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+
     }
 }
---
+++ Train_Stations/app/src/main/java/mobappdev/itm/fhj/at/train_stations/ICallBack.java
@@ -0,0 +1,8 @@
+package mobappdev.itm.fhj.at.train_stations;
+
+/**
+ * Created by Viktoria on 02.11.2015.
+ */
+public interface ICallBack {
+    public void handleJSonString(String jsonString);
+}
