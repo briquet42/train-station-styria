8a60a603980c94bb4c48c2db40251edbc1152961
vickytonal <vickylind@live.at>
2015-12-01 19:01:16 +0100

    doppelte Klassen entfernt

--- Train_Stations/app/src/main/java/mobappdev/itm/fhj/at/train_stations/ICallBack.java
+++
@@ -1,8 +0,0 @@
-package mobappdev.itm.fhj.at.train_stations;
-
-/**
- * Created by Viktoria on 02.11.2015.
- */
-public interface ICallBack {
-    public void handleJSonString(String jsonString);
-}
--- Train_Stations/app/src/main/java/mobappdev/itm/fhj/at/train_stations/ICallBackDistance.java
+++
@@ -1,8 +0,0 @@
-package mobappdev.itm.fhj.at.train_stations;
-
-/**
- * Created by Viktoria on 16.11.2015.
- */
-public interface ICallBackDistance {
-    public void handleJSonStringDistance(String jsonString);
-}
